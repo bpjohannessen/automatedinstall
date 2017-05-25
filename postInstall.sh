948f8ab2347b26c5c5a10a289141916aafb57b95
Bjørn-Petter Johannessen <bp.johannessen@gmail.com>
2017-05-25 12:26:01 +0300

    Fixed #1. Fixed #3

---
+++ postInstall.sh
@@ -0,0 +1,5 @@
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install git mkdocs pandoc python-pip vlc -y
+sudo mv /etc/xdg/autostart/at-spi-dbus-bus.desktop /etc/xdg/autostart/at-spi-dbus-bus.disabled
+pip install --upgrade pip
+pip install https://github.com/mitya57/python-markdown-math/archive/master.zip
